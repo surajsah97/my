d62518fed67d0dd650ec3473004228316f5fc781
Xaltam6 <xaltamgit@gmail.com>
2023-09-12 17:54:16 +0530

    started working on subscription module

--- controller/subscription_C.js
+++ controller/subscription_C.js
@@ -1,23 +1,23 @@
 var mongoose = require("mongoose");
 const constants = require("../models/modelConstants");
-const ProductModel = mongoose.model(constants.ProductModel);
+const SubModel = mongoose.model(constants.ProductModel);
 const common = require("../service/commonFunction");
 
 module.exports = {
-    addProduct: async (req, res) => {
+    addSub: async (req, res) => {
         try {
             if (req.files) {
                 // console.log(req.files)
                 req.body.productImage = `uploads/${req.files.productImage[0].originalname}`
             }
-            var find_prod = await ProductModel.findOne({ productName: req.body.productName });
+            var find_prod = await SubModel.findOne({ productName: req.body.productName });
             if (find_prod) {
                 return res.status(global.CONFIGS.responseCode.alreadyExist).json({
                     success: false,
                     message: global.CONFIGS.api.categoryalreadyadded,
                 })
             }
-            var create_prod = await ProductModel.create(req.body);
+            var create_prod = await SubModel.create(req.body);
             return res.status(global.CONFIGS.responseCode.success).json({
                 success: true,
                 message: global.CONFIGS.api.categoryadded,
@@ -32,16 +32,16 @@ module.exports = {
         }
     },
 
-    updateProduct: async (req, res) => {
+    updateSub: async (req, res) => {
         try {
-            var find_prod = await ProductModel.findOne({ productName: req.body.productName, _id: { $nin: [req.body.id] } });
+            var find_prod = await SubModel.findOne({ productName: req.body.productName, _id: { $nin: [req.body.id] } });
             if (find_prod) {
                 return res.status(global.CONFIGS.responseCode.alreadyExist).json({
                     success: false,
                     message: global.CONFIGS.api.categoryalreadyadded,
                 })
             }
-            var update_prod = await ProductModel.updateOne({ _id: req.body.id }, req.body);
+            var update_prod = await SubModel.updateOne({ _id: req.body.id }, req.body);
             return res.status(global.CONFIGS.responseCode.success).json({
                 success: true,
                 message: global.CONFIGS.api.categoryUpdated,
@@ -55,9 +55,9 @@ module.exports = {
         }
     },
 
-    deleteProduct: async (req, res) => {
+    deletesub: async (req, res) => {
         try {
-            var delete_prod = await ProductModel.deleteOne({ _id: req.params.id });
+            var delete_prod = await SubModel.deleteOne({ _id: req.params.id });
             if (delete_prod) {
                 return res.status(global.CONFIGS.responseCode.alreadyExist).json({
                     success: true,
---
+++ route/subscription_R.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router();
+const path = require("path")
+const cat = require("../controller/product_C")
+const Auth = require("../middleware/auth");
+const multer = require("multer");
+
+const localStorage = multer.diskStorage({
+    destination: (req, res, next) => {
+        next(null, path.join(__dirname, '../public/uploads'))
+    },
+    filename: (req, file, next) => {
+        next(null, file.originalname)
+    }
+});
+var upload1 = multer({ storage: localStorage });
+/* GET home page. */
+var cpUpload = upload1.fields([
+    { name: 'productImage', maxCount: 1 },
+])
+
+router.post("/addProduct", cpUpload, cat.addProduct)
+router.post("/updateProduct", cpUpload, cat.updateProduct)
+
+
+module.exports = router;
