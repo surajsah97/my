594a2ab12ca12930878ba7fcd3c631ae5a21efcf
mdirshad0477 <37357972+mdirshad0477@users.noreply.github.com>
2024-02-28 11:10:59 +0530

    chngws in some files

--- controller/deliveryLocation_C.js
+++ controller/deliveryLocation_C.js
@@ -1,30 +1,30 @@
 var mongoose = require("mongoose");
 const constants = require("../models/modelConstants");
-const BikeBrandModel = mongoose.model(constants.BikeBrandModel);
+const DeliveryLocationModel = mongoose.model(constants.DeliveryLocationModel);
 var customError = require('../middleware/customerror');
 
 module.exports = {
     addlocation: async (req, res, next) => {
-        var find_brand = await BikeBrandModel.findOne({ bikeBrand: req.body.bikeBrand });
-        if (find_brand) {
+        var find_location = await DeliveryLocationModel.findOne({ location: req.body.location });
+        if (find_location) {
             const err = new customError(global.CONFIGS.api.brandalreadyadded, global.CONFIGS.responseCode.alreadyExist);
             return next(err);
         }
-        var create_brand = await BikeBrandModel.create(req.body);
+        var create_location = await DeliveryLocationModel.create(req.body);
         return res.status(global.CONFIGS.responseCode.success).json({
             success: true,
             message: global.CONFIGS.api.brandadded,
-            data: create_brand
+            data: create_location
         })
     },
 
     updatelocation: async (req, res, next) => {
-        var find_brand = await BikeBrandModel.findOne({ bikeBrand: req.body.bikeBrand, _id: { $nin: [req.params.id] } });
-        if (find_brand) {
+        var find_location = await DeliveryLocationModel.findOne({ location: req.body.location, _id: { $nin: [req.params.id] } });
+        if (find_location) {
             const err = new customError(global.CONFIGS.api.brandalreadyadded, global.CONFIGS.responseCode.alreadyExist);
             return next(err);
         }
-        var update_brand = await BikeBrandModel.updateOne({ _id: req.params.id }, req.body);
+        var update_location = await DeliveryLocationModel.updateOne({ _id: req.params.id }, req.body);
         return res.status(global.CONFIGS.responseCode.success).json({
             success: true,
             message: global.CONFIGS.api.brandUpdated,
@@ -32,25 +32,25 @@ module.exports = {
     },
 
     locationList: async (req, res, next) => {
-        var find_brand = await BikeBrandModel.find({}).sort({ bikeBrand: 1 });
+        var find_location = await DeliveryLocationModel.find({}).sort({ location: 1 });
         return res.status(global.CONFIGS.responseCode.success).json({
             success: true,
             message: global.CONFIGS.api.getBrandSuccess,
-            data: find_brand
+            data: find_location
         })
     },
 
     locationListFront: async (req, res, next) => {
-        var find_brand = await BikeBrandModel.find({ activeStatus: "Active" }).sort({ location: 1 });
+        var find_location = await DeliveryLocationModel.find({ activeStatus: "Active" }).sort({ location: 1 });
         return res.status(global.CONFIGS.responseCode.success).json({
             success: true,
             message: global.CONFIGS.api.getBrandSuccess,
-            data: find_brand
+            data: find_location
         })
     },
 
     locationDelete: async (req, res, next) => {
-        var delete_brand = await BikeBrandModel.deleteOne({ _id: req.params.id });
+        var delete_location = await DeliveryLocationModel.deleteOne({ _id: req.params.id });
         return res.status(global.CONFIGS.responseCode.success).json({
             success: true,
             message: global.CONFIGS.api.brandDelete,
