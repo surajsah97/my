60079659bfd6c90b114adc50c3a860d3533224e8
Xaltam6 <xaltamgit@gmail.com>
2024-03-01 10:29:49 +0530

    changes in user api

--- route/adminRoute/TrialUsers_RA.js
+++ route/adminRoute/TrialUsers_RA.js
@@ -2,14 +2,15 @@ const express = require('express')
 const router = express.Router();
 const TrialUsers=require("../../controller/trialUser_C");
 const errorfun = require("../../middleware/catchAsyncErrors");
+const Auth = require("../../middleware/auth")
 
 router
   .route("/")
-  .get( errorfun(TrialUsers.trialusersListAdmin));
+  .get(Auth.adminValidateToken, errorfun(TrialUsers.trialusersListAdmin));
  
 router
-  .route("/:date")
-  .get( errorfun(TrialUsers.trialusersListAdminBYdate));
+  .route("/alluser")
+  .get(Auth.adminValidateToken, errorfun(TrialUsers.trialuserListall));
  
 
 module.exports = router;
